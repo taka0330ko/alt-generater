762b2e3245d8ee0018dabbb4ee07e69504994282
Taka0330-jp <gtakanari0330@gmail.com>
2026-04-11 12:52:22 -0700

    feat:initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,24 @@
-// 「起動処理」だけを書く
-import './style.css'
+// Entry point for browser-side initialization.
 
+const dragZone = document.getElementById('container')
+console.log(dragZone);
+
+
+function dragAndDrop() {
+    window.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        dragZone?.classList.add('drag-overlay')
+    })
+    window.addEventListener('dragleave', () => {
+        dragZone?.classList.remove('drag-overlay')
+    })
+
+    window.addEventListener("drop", (e) => {
+        e.preventDefault();
+        const files = e.dataTransfer?.files;
+        dragZone?.classList.remove('drag-overlay')
+        console.log(files);
+
+    });
+}
+dragAndDrop();
